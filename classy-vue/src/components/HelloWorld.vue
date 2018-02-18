564a7ea47a58d8e28384316ec67a605485f23565
doriholm <doriholm@gmail.com>
2018-02-18 10:13:17 +0100

    Vue setup and all dependencies installed

--- classy-vue/src/components/HelloWorld.vue
+++ classy-vue/src/components/HelloWorld.vue
@@ -95,19 +95,13 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
+<style lang="scss" scoped>
+
+ul{
   list-style-type: none;
-  padding: 0;
+  li{
+    display: inline;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
